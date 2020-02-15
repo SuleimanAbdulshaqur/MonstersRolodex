033b54db8ef6f338aa2dc05fb104832616ffd498
Suleiman <suleimanmadani@gmail.com>
2020-02-15 09:27:07 -0800

    Updates

---
+++ precache-manifest.a0868685bdb9a8e99a154d76e560bbce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9b2b13983f21954ff077618e0456be",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "8baddb7767f4251e6ef2",
+    "url": "/MonstersRolodex/static/css/main.6f20ba8e.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/MonstersRolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/MonstersRolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8baddb7767f4251e6ef2",
+    "url": "/MonstersRolodex/static/js/main.e073b55a.chunk.js"
+  },
+  {
+    "revision": "e552e97b424c9ea90834",
+    "url": "/MonstersRolodex/static/js/runtime-main.554b6087.js"
+  }
+]);
